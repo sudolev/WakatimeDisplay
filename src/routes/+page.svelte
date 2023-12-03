3ab3ec8d27f1a9ec3a258b9d50dfeb6ba22519b7
levtus <i@alexander.systems>
2023-12-03 13:27:02 -0600

    Build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
-jQuery.ajax({
-  type: 'GET',
-  url: 'https://wakatime.com/share/@alexsnder/3ead78ef-9185-48b8-93c3-ff67e18626f8.json',
-  dataType: 'jsonp',
-  success: function(response: any) {
-    console.log(response.data);
-  },
-});
 </script>
 
 <main>
     <div>
-        WEE
+        <!--<p>Total Days: {allTime.range.days_including_holidays.toString}</p>
+        <p>Total Time: {allTime.grand_total.human_readable_total_including_other_language}</p>
+        <p>Daily Average: {allTime.grand_total.human_readable_daily_average_including_other_language}</p>-->
+        <figure style="width: 60%"><embed src="https://wakatime.com/share/@alexsnder/61624052-9d5a-4098-9f1b-c4571c52fe3b.svg"></figure>
     </div>
-
 </main>
