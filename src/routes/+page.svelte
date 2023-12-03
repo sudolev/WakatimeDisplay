f4da9e2bcbf592e59ac93943f3cee056e88574bc
levtus <i@alexander.systems>
2023-12-03 13:26:24 -0600

    Add embed

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+jQuery.ajax({
+  type: 'GET',
+  url: 'https://wakatime.com/share/@alexsnder/3ead78ef-9185-48b8-93c3-ff67e18626f8.json',
+  dataType: 'jsonp',
+  success: function(response: any) {
+    console.log(response.data);
+  },
+});
+</script>
+
+<main>
+    <div>
+        WEE
+    </div>
+
+</main>
